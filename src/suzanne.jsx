e15346683cad89f57b52ab8506b71f7db17e28e9
Florian <git@florian-a.dev>
2023-03-23 22:00:22 +0100

    On va aller dancer le disco !

---
+++ src/suzanne.jsx
@@ -0,0 +1,23 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+import suzanne from "./suzanne.gltf?url"
+
+
+export default function Suzanne(props) {
+  const { nodes, materials } = useGLTF(suzanne);
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Suzanne.geometry}
+        material={nodes.Suzanne.material}
+        position={[0, 0.19, -0.04]}
+        onClick={() => alert("test")}
+      />
+    </group>
+  );
+}
+
+// useGLTF.preload("./assets/models/suzanne.glb");
