7b9a6353bd6a3213ee27e100caa63f29e8ab8cfb
Florian <git@florian-a.dev>
2024-05-22 18:10:24 +0200

    Rushing test

---
+++ src/Vivatech.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Vivatech = () => {
+  return (
+    <>
+      <div className="container d-flex flex-column justify-content-center align-items-center vh-100">
+        <img src={`/itstrape.jpg`} alt="Vivatech" className="img-fluid" style={{ maxHeight: '50vh', maxWidth: '100%' }} />
+      </div>
+      <p>Infos
+        Hey 🤗 !
+
+        Je cherche un stage de 4 à 6 mois dans une entreprise blockchain ou une entreprise plus traditionnelle qui souhaite transitionner vers le web3.</p>
+    </>
+  );
+}
+
+export default Vivatech;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Scene";
+import App from "./Vivatech";
 
 ReactDOM.render(
   <React.StrictMode>
