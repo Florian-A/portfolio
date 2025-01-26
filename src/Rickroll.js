f9690b6522b2d6975ac6b2c7ba1ec01356ecd0e1
Florian <git@florian-a.dev>
2025-01-26 12:04:44 +0100

    Update Rickroll.js

--- src/Rickroll.js
+++ src/Rickroll.js
@@ -22,14 +22,10 @@ const Rickroll = () => {
 
         <div className="text-overlay">
           <div className="text-container">
-            <p className="text-background">
-              Vous vous attendiez à quoi ? 🤣
-            </p>
             <div className="mt-4">
               <button onClick={() => window.open("https://linkedin.com/in/florian-a-dev", "_blank")} className="btn btn-primary mx-2"><FaLinkedin /></button>
               <button onClick={() => window.open("https://github.com/florian-a", "_blank")} className="btn btn-primary mx-2"><FaGithub /></button>
               <a href="mailto:linkedin@florian-a.dev?subject=Rickroll&body=N'hésitez pas à m'envoyer un petit message !" className="btn btn-primary mx-2"><FaEnvelope /></a>
-
             </div>
           </div>
         </div>
